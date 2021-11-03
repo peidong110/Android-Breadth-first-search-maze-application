3ad895be141a0755a8872b9cfe43df86bf76b490
mrspoon119 <paytonpei@cmail.carleton.ca>
2021-11-02 21:23:51 -0400

    disable rotation

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
 import android.os.AsyncTask;
@@ -18,7 +16,6 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -32,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     int[][]  matrix = new int[NUM_ROWS][NUM_COLS];
     int flg;
     private Button solveMazeButton;
+    boolean status;
     ArrayList<Character> a;
     //private Maze model; //not needed for tutorial
     public Boolean hasItem(String str){
@@ -48,55 +46,40 @@ public class MainActivity extends AppCompatActivity {
 
         return flag;
     }
-    public class updateButtonTask  extends AsyncTask<Void, Void, Void>{
+    public class updateButtonTask  extends AsyncTask<Void, Void, Boolean>{
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Boolean doInBackground(Void... voids) {
             int[] arr = getCoordinates('S');
 //            System.out.println(Arrays.toString(arr));
             int[] arr1 = getCoordinates('D');
 //            System.out.println(Arrays.toString(arr1));
-            matrix[0][0] =1;
+            matrix[arr[0]][arr[1]] = 1;
             System.out.println(Arrays.toString(arr1));
-//
-//            System.out.println(Arrays.deepToString(keys));
+
             int k = 0;
-
-
-
-//
-//            System.out.println("END: X: "+arr1[0]+" Y: "+arr1[1]);//3:1
-//            System.out.println("FLAG: "+flg);
-//            System.out.println("Matrix: "+matrix[3][1]);
-//            System.out.println("keys:  "+keys[3][1]);
-//            System.out.println("MAXTRIX:::\n");
-//            System.out.println(Arrays.deepToString(keys));
-
-
-            System.out.println("Array1: X: "+arr1[0]+" Y: "+arr1[1]);//9,9, (2,2)=>test
-            System.out.println("Matrix Destination: "+matrix[arr1[0]][arr1[1]]);
-            //TEST CASE: WHEN THERE ARE NO WALLS, IT WORKS FINE.
-            //TEST CASE: WHEN OBSTACLES BLOCKS
-                while (matrix[arr1[0]][arr1[1]] == 0)
-                {
-                    k +=1;
-                    System.out.println("K is: "+k+" and it's passed in to the function");
-                    boolean b = singleMove(k);
-                    sleep(1);
-                    if (!b)
-                        break;
-
+            while (matrix[arr1[0]][arr1[1]] == 0)
+            {
+                k +=1;
+                System.out.println("K is: "+k+" and it's passed in to the function");
+                boolean b = singleMove(k);
+//                    sleep(1);
+                if (!b)
+                    break;
                 }
-                System.out.println("WHILE LOOP ENDED");
-//
-//            singleMove(1);
-//            singleMove(2);
+            System.out.println("WHILE LOOP ENDED");
 
-//            System.out.println("A: items");
-//            System.out.println(a);
-//            display();
-            return null;
+            return false;
         }
+        public void onPostExecute(Boolean result){
+            super.onPostExecute(result);
+            solveMazeButton.setText(R.string.solve);
+            buttonState(true);
+            reset();
+            status = true;
+
+        }
+
     }
     public void updateBorder(Button b){
         ShapeDrawable shapedrawable = new ShapeDrawable();
@@ -117,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public int[] getCoordinates(char c){
-        int[] arr = {-1,-1};
+        int[] arr = {-1,-100};
         for (int i = 0;i<NUM_ROWS;i++){
             for (int j = 0;j<NUM_COLS;j++){
                 if (keys[i][j] == c){
@@ -143,26 +126,12 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    public void generateGameBoard(){
-        TableLayout gameLayout = findViewById(R.id.gameTable);
-        TableRow tr;
-        for(int i =0;i<NUM_ROWS;i++){
-            tr = new TableRow(MainActivity.this);
-            for(int j =0;j<NUM_COLS;j++){
-                Button btn = new Button(MainActivity.this);
-                btn.setText("E");
-                btn.setBackgroundColor(Color.WHITE);
-                updateBorder(btn);
-                keys[i][j] = 'E';
-
-                TableRow.LayoutParams p = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                p.weight =1;
-                btn.setLayoutParams(p);
-                buttons[i][j] = btn;
-                tr.addView(btn);
+    public void buttonListener(){
+        for (int i =0;i<NUM_ROWS;i++){
+            for (int j =0;j<NUM_COLS;j++){
                 int finalI = i;
                 int finalJ = j;
-                btn.setOnClickListener(new View.OnClickListener() {
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
                     @SuppressLint("ResourceAsColor")
                     @Override
                     public void onClick(View view) {
@@ -197,36 +166,41 @@ public class MainActivity extends AppCompatActivity {
                             b.setText("W");
                             b.setBackgroundColor(Color.YELLOW);
                             keys[finalI][finalJ] = 'W';
-
-
                         }
                         else if(buttonText.equals("W")){
                             b.setText("E");
                             b.setBackgroundColor(Color.WHITE);
                             updateBorder(b);
                             keys[finalI][finalJ] = 'E';
-
-
-
                         }
-
-
                     }
                 });
             }
+        }
+    }
+    public void generateGameBoard(){
+        TableLayout gameLayout = findViewById(R.id.gameTable);
+        TableRow tr;
+        for(int i =0;i<NUM_ROWS;i++){
+            tr = new TableRow(MainActivity.this);
+            for(int j =0;j<NUM_COLS;j++){
+                Button btn = new Button(MainActivity.this);
+                btn.setText("E");
+                btn.setBackgroundColor(Color.WHITE);
+                updateBorder(btn);
+                keys[i][j] = 'E';
+                TableRow.LayoutParams p = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                p.weight =1;
+                btn.setLayoutParams(p);
+                buttons[i][j] = btn;
+                tr.addView(btn);
+            }
             gameLayout.addView(tr);
-
-        }
-
-    }
-    public void display(){
-        for (int i = 0;i<10;i++){
-            System.out.println(Arrays.toString(matrix[i]));
         }
     }
-    public void sleep(int period){
+    public void sleep(int interval){
         try {
-            Thread.sleep(200);
+            Thread.sleep(interval);
         } catch(InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
@@ -241,7 +215,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public boolean singleMove(int round){
         boolean flg1 = false;
-
         for (int i = 0;i<10;i++){
             for (int j =0;j<10;j++){
                 if (matrix[i][j] == round){
@@ -249,39 +222,50 @@ public class MainActivity extends AppCompatActivity {
                     flg1 = true;
                     if (i > 0 && matrix[i-1][j] == 0 && keys[i-1][j] != 'W'){
                         matrix[i-1][j] = round+1;
-                        buttons[i-1][j].setBackgroundColor(Color.GREEN);
-                        sleep(11);
+                        if (keys[i-1][j] == 'E'){
+                            buttons[i-1][j].setBackgroundColor(Color.GREEN);
 
+                        }
+                        sleep(200);
                     }
                     if (j>0 && matrix[i][j-1] == 0 && keys[i][j-1] != 'W'){
                         matrix[i][j-1] = round+1;
-                        buttons[i][j-1].setBackgroundColor(Color.GREEN);
-                        sleep(11);
+                        if (keys[i][j-1] == 'E'){
+                            buttons[i][j-1].setBackgroundColor(Color.GREEN);
+                        }
+                        sleep(200);
                     }
                     if (i < 9 && matrix[i+1][j] == 0 && keys[i+1][j] != 'W' ){
                         matrix[i+1][j] = round+1;
-                        buttons[i+1][j].setBackgroundColor(Color.GREEN);
-                        sleep(11);
+                        if (keys[i+1][j] == 'E'){
+                            buttons[i+1][j].setBackgroundColor(Color.GREEN);
+                        }
+                        sleep(200);
 
                     }
                     if (j < 9 && matrix[i][j+1] ==0 &&  keys[i][j+1] != 'W' ){
                         matrix[i][j+1] = round+1;
-                        buttons[i][j+1].setBackgroundColor(Color.GREEN);
-                        sleep(11);
-
+                        if (keys[i][j+1] == 'E')
+                        {
+                            buttons[i][j+1].setBackgroundColor(Color.GREEN);
+                        }
+                        sleep(200);
                     }
                 }
-
-
-
             }
-
-
         }
         dis(matrix);
         return flg1;
-//        System.out.println(Arrays.deepToString(matrix));
     }
+
+    public void buttonState(boolean flg){
+        for (int i =0;i< NUM_COLS;i++){
+            for (int j=0;j<NUM_COLS;j++){
+                buttons[i][j].setEnabled(flg);
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -290,6 +274,9 @@ public class MainActivity extends AppCompatActivity {
         initArr();//init arr and assign x to each item in the array.
         flg = 0;
         a = new ArrayList<>();
+        buttonListener();
+
+        status = true;
         solveMazeButton = (Button) findViewById(R.id.button_solve_maze);
         solveMazeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -298,15 +285,23 @@ public class MainActivity extends AppCompatActivity {
                 Toast t = Toast.makeText(MainActivity.this, b.getText(), Toast.LENGTH_SHORT);
                 t.show();
                 if (hasItem("S")&& hasItem("D")){
+
                     //Once we have S and D, we can now start to solve the maze. If you do not want a wall, then it's fine.
                     System.out.println("Yes, we have start and des, we can start to solve the maze.");
                     //get start and end coordinates here and start path finding.
-                    updateButtonTask updateButtonTask = new updateButtonTask();
-                    updateButtonTask.execute();
-                    System.out.println("end of ");
+                    if (status){
+                        System.out.println("The button text is: "+solveMazeButton.getText().toString());
+                        System.out.println("In status");
+                        updateButtonTask updateButtonTask = new updateButtonTask();
+                        AsyncTask<Void, Void, Boolean> a = updateButtonTask.execute();
+                        System.out.println("X1"+a);
+                        buttonState(false);
+                        System.out.println();
+                    }
+                    else {
+                        System.out.println("Not in status");
 
-
-
+                    }
                 }
                 else{
 
